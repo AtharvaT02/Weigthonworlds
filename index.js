af314887e95bac362544ca9bc99ca17a509007e8
AtharvaT02 <atharvathorkar2002@gmail.com>
2021-10-22 14:10:37 +0530

    sserd

---
+++ index.js
@@ -0,0 +1,224 @@
+var animateButton = function(e) {
+
+    e.preventDefault;
+    //reset animation
+    e.target.classList.remove('animate');
+    
+    e.target.classList.add('animate');
+    setTimeout(function(){
+      e.target.classList.remove('animate');
+    },700);
+  };
+  
+  var bubblyButtons = document.getElementsByClassName("bubbly-button");
+  
+  for (var i = 0; i < bubblyButtons.length; i++) {
+    bubblyButtons[i].addEventListener('click', animateButton, false);
+  }
+const sunX = window.innerWidth / 2 ;
+const sunY = window.innerHeight / 2;
+const sun = document.querySelector('.sun');
+sun.style.left = `${sunX - (0.01*window.innerWidth)}px`;
+sun.style.top = `${sunY - (0.01*window.innerWidth)}px`;
+sun.id=0;
+sun.name = "SUN";
+sun.gforce = 274;
+const mercury = {
+    name: "MERCURY",
+    gforce: 3.7,
+    width: 1,
+    speed: 0.005,
+    theta: Math.random() * 2 * Math.PI,
+    radius:  window.innerWidth * 0.02,
+    el: document.querySelector('.mercury')
+}
+const venus = {
+    name: "Venus",
+    gforce: 8.8,
+    width: 1.5,
+    speed: -0.004,
+    theta: Math.random() * 2 * Math.PI,
+    radius:  window.innerWidth * 0.035,
+    el: document.querySelector('.venus')
+}
+const earth = {
+    name:'EARTH',
+    gforce: 9.8,
+    width:1.6, 
+    speed: 0.003,
+    theta: Math.random() * 2 * Math.PI,
+    radius: window.innerWidth * 0.055,
+    el: document.querySelector('.earth')
+}
+const mars = {
+    name: "MARS",
+    gforce: 3.7,
+    width: 1.5,
+    speed: 0.0025,
+    theta: Math.random() * 2 * Math.PI,
+    radius: window.innerWidth * 0.075,
+    el: document.querySelector('.mars')
+}
+const jupiter = {
+    name: "JUPITER",
+    gforce: 24.7,
+    width: 2,
+    speed: 0.002,
+    theta: Math.random() * 2 * Math.PI,
+    radius: window.innerWidth * 0.12,
+    el: document.querySelector('.jupiter')
+}
+const saturn = {
+    name: "SATURN",
+    gforce: 10.5,
+    width: 1.8,
+    speed: 0.001,
+    theta: Math.random() * 2 * Math.PI,
+    radius: window.innerWidth * 0.15,
+    el: document.querySelector('.saturn')
+}
+const uranus = {
+    name: "URANUS",
+    gforce: 9,
+    width: 1.7,
+    speed: 0.0009,
+    theta: Math.random() * 2 * Math.PI,
+    radius: window.innerWidth * 0.17,
+    el: document.querySelector('.uranus')
+}
+const neptune = {
+    name: "NEPTUNE",
+    gforce: 11.7,
+    width: 1.6,
+    speed: 0.00085,
+    theta: Math.random() * 2 * Math.PI,
+    radius: window.innerWidth * 0.19,
+    el: document.querySelector('.neptune')
+}
+const orbit1 = {
+    width: 4,
+    speed: 0,
+    theta: 0,
+    radius: 0,
+    el: document.querySelector('.orbit1')
+}
+const orbit2 = {
+    width: 7,
+    speed: 0,
+    theta: 0,
+    radius: 0,
+    el: document.querySelector('.orbit2')
+}
+const orbit3 = {
+    width: 11,
+    speed: 0,
+    theta: 0,
+    radius: 0,
+    el: document.querySelector('.orbit3')
+}
+const orbit4 = {
+    width: 15,
+    speed: 0,
+    theta: 0,
+    radius: 0,
+    el: document.querySelector('.orbit4')
+}
+const orbit5 = {
+    width: 24,
+    speed: 0,
+    theta: 0,
+    radius: 0,
+    el: document.querySelector('.orbit5')
+}
+const orbit6 = {
+    width: 30,
+    speed: 0,
+    theta: 0,
+    radius: 0,
+    el: document.querySelector('.orbit6')
+}
+const orbit7 = {
+    width: 34,
+    speed: 0,
+    theta: 0,
+    radius: 0,
+    el: document.querySelector('.orbit7')
+}
+const orbit8 = {
+    width: 38,
+    speed: 0,
+    theta: 0,
+    radius: 0,
+    el: document.querySelector('.orbit8')
+}
+const astroid = {
+    width: 20,
+    speed: 0,
+    theta: 0,
+    radius: 0,
+    el: document.querySelector('.astroid')
+}
+const suno = {
+    name: "SUN",
+    gforce: 274,
+    width: 0,
+    speed: 0,
+    theta: 0,
+    radius: 0,
+    el: document.querySelector('.solar')
+}
+const planets = [
+    suno,
+    mercury,
+    venus,
+    earth,
+    mars,
+    jupiter,
+    saturn,
+    uranus,
+    neptune, 
+    orbit1,
+    orbit2,
+    orbit3,
+    orbit4,
+    orbit5,
+    orbit6,
+    orbit7,
+    orbit8,
+    astroid,
+]
+function update(planet) {
+    planet.theta += planet.speed;
+    planet.el.style.left = `${Math.cos(planet.theta) * planet.radius +sunX - ((planet.width*window.innerWidth)/200)}px`;
+    planet.el.style.top = `${Math.sin(planet.theta) * planet.radius +sunY - ((planet.width*window.innerWidth)/200)}px`;
+}
+var cli=1;
+var g1=0;
+var g1=0;
+
+function myFunction(clicked_id){
+    for(let i = 0 ; i < 9 ; i++){
+        if(clicked_id == i){
+            if(cli == 1){
+                document.getElementById("planet1").innerHTML = planets[i].name +"<br><br>";
+                gf1 = planets[i].gforce;
+                cli++;
+            }else if(cli == 2){
+                document.getElementById("planet2").innerHTML = planets[i].name +"<br><br>";
+                gf2 = planets[i].gforce;
+                cli++;
+            }
+            
+        }
+    }
+}
+function calculate(){
+    var weight = $("#pl1").val();
+    weight = weight*gf2/gf1;
+    $("#pl2").val(weight);
+}
+
+
+setInterval(() => {
+    planets.forEach(update);
+},10)
